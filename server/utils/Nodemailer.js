4ef5e2ddd9f3c986fdc63234dbb4d979bcd19b11
hlwsatyam <satyampandit021@gmail.com>
2025-03-28 13:07:27 +0530

    "change nodemailer"

--- server/utils/Nodemailer.js
+++ server/utils/Nodemailer.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'swhealthcare17@gmail.com', // Replace with your email
-        pass: 'nkho mnqg iaur hjaf',    // Replace with App Password
+        user:  process.env.USER, // Replace with your email
+        pass:    process.env.PASS,    // Replace with App Password
     }, // Output detailed logs
 });
 
